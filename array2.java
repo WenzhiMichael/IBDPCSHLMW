69ae62d65cbfd1c6eee1f03b047187158e2c01a2
WenzhiMichael <91454276+WenzhiMichael@users.noreply.github.com>
2021-10-04 00:27:05 +0700

    Array2

---
+++ array2.java
@@ -0,0 +1,16 @@
+package arrays;
+import java.util.Arrays;
+
+public class array2 {
+    public  static  void  main (String [] args) {
+
+        int array[]={4,1,59,23,41};
+        Arrays.sort(array);
+        for(int i=0;i<=array.length-1;i++){
+
+        System.out.println(array[i]);
+        }
+
+    }
+
+}
